33d92d7b4b36fc5d2824b5086bc20a482a5c9d47
Aravindhan V <aravindhan.v@perfios.com>
2021-09-07 21:30:12 +0530

    REFACTOR: Moved input validation into functions

--- src/com/bridgelabz/regex/UserRegistration.java
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -5,37 +5,70 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+	public static boolean validateName(String name) {
+		Pattern pattern;
+		pattern = Pattern.compile("^[A-Z][a-z]{2,}");
+		return pattern.matcher(name).matches();
+	}
+	
+	public static boolean validateEmail(String email) {
+		Pattern pattern = Pattern.compile("abc(\\.xyz)?@bl\\.co(\\.in)?");
+		return pattern.matcher(email).matches();
+	}
+	
+	public static boolean validatePassword(String password) {
+		Pattern pattern = Pattern.compile(".{7,}");
+		if(!pattern.matcher(password).matches()) {
+			return false;
+		}
+		
+		pattern = Pattern.compile(".*[A-Z].*");
+		if (!pattern.matcher(password).matches()) {
+			return false;
+		}
+		
+		pattern = Pattern.compile(".*\\d.*");
+		if (!pattern.matcher(password).matches()) {
+			return false;
+		}
+		
+		pattern = Pattern.compile("^[a-zA-Z0-9]*[^a-zA-Z0-9][a-zA-Z0-9]*$");
+		if (!pattern.matcher(password).matches()) {
+			return false;
+		}
+		
+		return true;
+	}
+	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		Pattern pattern;
+		
 		String firstName,lastName,email,password;
 		Matcher matcher;
 
-		pattern = Pattern.compile("^[A-Z][a-z]{2,}");
+		
 		System.out.println("Enter the FirstName");
 		while (true) {
 			firstName = scanner.nextLine();
-			if (pattern.matcher(firstName).matches()) {
+			if (validateName(firstName)) {
 				break;
 			}
 			System.out.println("Enter valid first name");
 		}
 		
-		pattern = Pattern.compile("^[A-Z][a-z]{2,}");
 		System.out.println("Enter the Last Name");
 		while (true) {
 			lastName = scanner.nextLine();
-			if (pattern.matcher(lastName).matches()) {
+			if (validateName(lastName)) {
 				break;
 			}
 			System.out.println("Enter valid last name");
 		}
-		
-		pattern = Pattern.compile("abc(\\.xyz)?@bl\\.co(\\.in)?");
+			
 		System.out.println("Enter the email");
 		while (true) {
 			email = scanner.nextLine();
-			if (pattern.matcher(email).matches()) {
+			if (validateEmail(email)) {
 				break;
 			}
 			System.out.println("Enter valid email");
@@ -44,29 +77,10 @@ public class UserRegistration {
 		System.out.println("Enter the password");
 		while (true) {
 			password = scanner.nextLine();
-			pattern = Pattern.compile(".{7,}");
-			if (!pattern.matcher(password).matches()) {
-				System.out.println("Enter valid password");
-				continue;
+			if (validatePassword(password)) {
+				break;
 			}
-			
-			pattern = Pattern.compile(".*[A-Z].*");
-			if (!pattern.matcher(password).matches()) {
-				System.out.println("Enter valid password");
-				continue;
-			}
-			
-			pattern = Pattern.compile(".*\\d.*");
-			if (!pattern.matcher(password).matches()) {
-				System.out.println("Enter valid password");
-				continue;
-			}
-			
-			pattern = Pattern.compile("^[a-zA-Z0-9]*[^a-zA-Z0-9][a-zA-Z0-9]*$");
-			if (!pattern.matcher(password).matches()) {
-				System.out.println("Enter valid password");
-				continue;
-			}
+			System.out.println("Enter valid password");
 		}
 	}
 }
